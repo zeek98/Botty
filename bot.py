e77a4707b34a54bc3004fc4d17c31901e751e4cb
Zeek98 <68119903+zeek98@users.noreply.github.com>
2023-11-19 23:46:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the Word document – hang tight! This may take a moment."):
         # Use the DocxReader to load data from Word documents
         loader = DocxReader()
-        documents = loader.load_data(file=Path('./Task Nomenclature.docx'))
+        documents = loader.load_data(file='./Task Nomenclature.docx')
         
         # Additional processing if needed
         
