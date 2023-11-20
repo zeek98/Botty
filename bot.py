e5fc42db7e0eb9cf1b3b41bfe248d64d39675c08
Zeek98 <68119903+zeek98@users.noreply.github.com>
2023-11-20 15:16:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
+from pathlib import Path
+from llama_index import VectorStoreIndex, ServiceContext, Document, download_loader
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
@@ -7,8 +8,8 @@ from llama_index import SimpleDirectoryReader
 st.set_page_config(page_title="Chat with the DP bot, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Chat with the DP, powered by LlamaIndex 💬🦙")
 st.info("To help go beyond and above", icon="📃")
-         
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about the nomenclature!"}
     ]
@@ -16,31 +17,53 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the nomenclature – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
+        # Determine the source based on a condition or prompt
+        source_type = "md"  # Default to Markdown
+        prompt = st.session_state.messages[-1]["content"].lower() if st.session_state.messages else ""
+
+        # Change the source type based on the presence of a keyword in the prompt
+        if "estimate" in prompt:
+            source_type = "xlsx"
+
+        if source_type == "md":
+            # Load data from Markdown files
+            reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+            docs = reader.load_data()
+        elif source_type == "xlsx":
+            # Load data from Excel files
+            PandasExcelReader = download_loader("PandasExcelReader")
+            loader = PandasExcelReader(pandas_config={"header": 0})
+            docs = loader.load_data(file=Path('./data.xlsx'))
+        else:
+            # Default to Markdown if the condition is not met
+            reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+            docs = reader.load_data()
+
         service_context = ServiceContext.from_defaults(
-        chunk_size=512, chunk_overlap=50, llm=OpenAI(model="gpt-3.5-turbo", temperature=0.2, system_prompt="You will give the answers truthfully and do not deviate but if you don't know then say I dont Know as the answer."))
+            chunk_size=512, chunk_overlap=50, llm=OpenAI(model="gpt-3.5-turbo", temperature=0.2, system_prompt="You will give the answers truthfully and do not deviate, but if you don't know then say I don't Know as the answer.")
+        )
+
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         query_engine = index.as_query_engine(similarity_top_k=2)
         return index
 
 index = load_data()
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+if "chat_engine" not in st.session_state.keys():
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If last message is not from assistant, generate a new response
+# If the last message is not from the assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+            st.session_state.messages.append(message)
