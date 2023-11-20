6ed823f8f3dcdbb423bd8dfb9e10434d8e3b21c7
Zeek98 <68119903+zeek98@users.noreply.github.com>
2023-11-20 12:24:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,8 +18,11 @@ def load_data():
     with st.spinner(text="Loading and indexing the nomenclature – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(
+        chunk_size=512, chunk_overlap=50)
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.1, system_prompt="You will give the answers truthfully and do not deviate but if you don't know then say I dont Know as the answer."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        query_engine = index.as_query_engine(similarity_top_k=4)
         return index
 
 index = load_data()
